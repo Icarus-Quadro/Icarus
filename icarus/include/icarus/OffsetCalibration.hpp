8f5806c3c47bd7975483b73a9b094799d2f9a0d7
Roman Chomik <romanchom@gmail.com>
2019-03-22 10:54:33 +0100

    Reorganized project

---
+++ icarus/include/icarus/OffsetCalibration.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace icarus
+{
+    template<typename T, size_t N>
+    struct OffsetCalibration
+    {
+        explicit OffsetCalibration();
+        explicit OffsetCalibration();
+    };
+}
